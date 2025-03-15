6e311f188297c7b0afc464a145d5740be1b3508b
Noilty <thenoilty@gmail.com>
2025-03-15 16:08:28 +0700

    Refactored *

---
+++ constants.example.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+const FTP_HOST = '';
+const FTP_DIR = FTP_HOST . '/';
+const TG_CHANNEL = '';
+const TG_CHANNEL_BOT = '';
+const TG_CHANNEL_BOT_TOKEN = '';
+
+const POST_COUNT = 13;
